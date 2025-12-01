112a4116c021fb044ecd08eefc5791a0ccaef25a
md <texet0598@gmail.com>
2025-12-01 22:14:56 +0300

    feat: add password hashing

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,15 +1,48 @@
 use axum::{Json, extract::State, http::StatusCode};
-use sqlx::PgPool;
-use crate::models::auth::{CreateUserRequest, UserResponse, EmailLoginRequest, UsernameLoginRequest};
+use chrono::{DateTime, Utc};
+use sqlx::{PgPool, self};
+use crate::models::auth::{CreateUserRequest, UserResponse, EmailLoginRequest, UsernameLoginRequest, User};
+use serde_json::json;
+use uuid::Uuid;
+use bcrypt::{hash, DEFAULT_COST};
 
 
 pub async fn signup(
     State(pool): State<PgPool>,
     Json(payload): Json<CreateUserRequest>,
-) -> Result<(StatusCode, Json<UserResponse>), StatusCode> {
-    // Implementation here
-    Err(StatusCode::NOT_IMPLEMENTED)
+) -> Result<(StatusCode, Json<UserResponse>), (StatusCode, String)> {
+    let password_hash = hash(&payload.password, 12)
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("Password hashing failed: {}", e)))?;
+
+    // Создаем пользователя в БД
+    let now = Utc::now();
+
+
+    let user = sqlx::query_as!(
+        User,
+        r#"
+            INSERT INTO users (username, email, password_hash, created_at)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *
+        "#,
+        &payload.username,
+        &payload.email,
+        &password_hash,
+        now,
+        
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?; 
+
+    // Преобразуем в Response
+    let user_response = UserResponse {
+        id: user.id,
+        username: user.username.to_string(),
+        email: user.email.to_string(),
+    };
     
+    return Ok((StatusCode::CREATED, Json(user_response)));
 }
 
 pub async fn login_with_username(
--- src/models/auth.rs
+++ src/models/auth.rs
@@ -5,20 +5,20 @@ use std::clone::Clone;
 use validator::{Validate};
 use chrono::{DateTime, Utc};
 
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct User {
     pub id: Uuid,
     pub username: String,
     pub email: String,
     pub password_hash: String,
-    pub email_verified: bool, 
-    pub first_name: String,
-    pub last_name: String,
-    pub avatar_url: String,
-    pub is_active: bool,
-    pub created_at: DateTime<Utc>,
-    pub last_login_at: DateTime<Utc>,
-
+    pub email_verified: Option<bool>, 
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub avatar_url: Option<String>,
+    pub is_active: Option<bool>,
+    pub created_at: Option<DateTime<Utc>>,
+    pub last_login_at: Option<DateTime<Utc>>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -50,5 +50,5 @@ pub struct UserResponse {
     pub id: Uuid,
     pub username: String,
     pub email: String,
-    pub created_at: DateTime<Utc>,
+    // pub created_at: DateTime<Utc>,
 }
