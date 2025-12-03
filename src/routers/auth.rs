d20b337ecacf5405a13075fe4594ec869e51d753
md <texet0598@gmail.com>
2025-12-03 20:13:08 +0300

    refactor: api path

--- src/routers/auth.rs
+++ src/routers/auth.rs
@@ -4,7 +4,7 @@ use sqlx::PgPool;
 
 pub fn routes() -> Router<PgPool> {
     return Router::new()
-        .route("/sign-up", post(signup))
-        .route("/email-sign-in", post(login_with_email))
-        .route("/username-sign-in", post(login_with_username))
+        .route("/api/sign-up", post(signup))
+        .route("/api/email-sign-in", post(login_with_email))
+        .route("/api/username-sign-in", post(login_with_username))
 }
